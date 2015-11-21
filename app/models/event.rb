ac36577958ca2757a941b701d71f21c746071e82
Dominik Helle <helle@omniscale.de>
2015-11-21 12:51:10 +0100

    load only our track we need for the next confernece

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,10 @@ class Event < ActiveRecord::Base
 
   before_create :generate_guid
 
-  TYPES = [:lecture, :workshop, :podium, :lightning_talk, :meeting, :film, :concert, :djset, :performance, :other]
+  # fix hack to load types from events
+  # next step load from conference settings
+  # TYPES = [:lecture, :workshop, :podium, :lightning_talk, :meeting, :film, :concert, :djset, :performance, :other]
+  TYPES = [:lecture, :workshop, :lightning_talk]
 
   has_one :ticket, dependent: :destroy
   has_many :conflicts_as_conflicting, class_name: "Conflict", foreign_key: "conflicting_event_id", dependent: :destroy
