0d655d22fa1ff938f7c76b274c9db6f25095b66d
Dominik Helle <helle@omniscale.de>
2015-11-21 10:36:24 +0100

    use other syntax for raty helper to use frab with ruby2.0 from ubuntu 14.04

--- app/helpers/raty_helper.rb
+++ app/helpers/raty_helper.rb
@@ -1,7 +1,7 @@
 module RatyHelper
   def raty_for_input(id, score_input_id, score_name)
     content_tag :div, id: id, class: 'rating', data: {
-      'raty-input': 'on',
+      'raty-input' => 'on',
       source: score_input_id,
       target: score_name
     }.merge(image_data) do
@@ -21,9 +21,9 @@ module RatyHelper
   def image_data
     {
       path: '',
-      'star-on': image_path('raty/star-on.png'),
-      'star-off': image_path('raty/star-off.png'),
-      'star-half': image_path('raty/star-half.png')
+      'star-on' => image_path('raty/star-on.png'),
+      'star-off' => image_path('raty/star-off.png'),
+      'star-half' => image_path('raty/star-half.png')
     }
   end
 end
